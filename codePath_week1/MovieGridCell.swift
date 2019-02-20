fc5fcffac0125b4908a807b5b3a9f412cd0f273d
Joy Wang <joy.wang312@gmail.com>
2019-02-19 21:54:48 -0800

    finishing week2 PA

---
+++ codePath_week1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  codePath_week1
+//
+//  Created by Joy_Wang on 2/19/19.
+//  Copyright © 2019 Joy_Wang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
